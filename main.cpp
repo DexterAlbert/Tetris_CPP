e04e162af2ee135b820d6853ae6f276b1b004e9a
DexterAlbert <2602147960zyb@gmail.com>
2026-06-18 01:03:02 +0800

    第一个程序：跑动的*符

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void gotoxy(int x,int y)
+{
+    COORD coord;
+    coord.X=x;
+    coord.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+}
+
+int main(){
+    for(int i=0;i<10;i++){
+        gotoxy(i*2,5);
+        cout<<"*";
+        Sleep(200);
+    }
+    return 0;
+}
