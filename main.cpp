3b4d86b8a8dff9a926ea016bf3be53c47b17a20a
DexterAlbert <2602147960zyb@gmail.com>
2026-06-18 23:32:31 +0800

    重新排版

--- main.cpp
+++ main.cpp
@@ -2,18 +2,20 @@
 #include <windows.h>
 using namespace std;
 
-void gotoxy(int x,int y)
+void gotoxy(int x, int y)
 {
     COORD coord;
-    coord.X=x;
-    coord.Y=y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-int main(){
-    for(int i=0;i<10;i++){
-        gotoxy(i*2,5);
-        cout<<"*";
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        gotoxy(i * 2, 5);
+        cout << "*";
         Sleep(200);
     }
     return 0;
